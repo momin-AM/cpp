3b525fa7cfa4bfc0da01cd1aae53d26c7c1e5860
Momin <sk4549151@gmail.com>
2025-10-12 20:01:15 +0530

    Update arr2D-problems.cpp

--- exercises/arr2D-problems.cpp
+++ exercises/arr2D-problems.cpp
@@ -305,3 +305,101 @@ using namespace std;
 //     }
 //     return 0;
 // }
+
+
+//prefix sum for a rectange of given two points(l1,r1),(l2,r2)
+
+int rectangleSum(vector <vector<int>> v, int l1,int r1, int l2,int r2){
+    int res=0;
+    for(int i=l1;l1<l2?i<=l2:i>=l2;i){
+        for(int j=r1;r1<r2?j<=r2:j>=r2;j){
+            res+=v[i][j];
+            if(r1>r2) j--;
+            else j++;
+        }
+        if(l1>l2){i--;
+        }else i++;
+    }
+    return res;
+}
+//method2 using prefix sum on rows
+
+vector <vector<int>> preSumRow(vector <vector<int>> v){
+    int x=v.size() , y =v[0].size();
+    for(int i=0;i<x;i++){
+        for(int j=1;j<y;j++){
+            v[i][j]+=v[i][j-1];
+        }
+    }
+    return v;
+}
+//method 3 using prefix sum on rows and colums
+
+void preSumColumn(vector <vector<int>> &v){
+    int x=v.size() , y =v[0].size();
+    for(int i=0;i<y;i++){
+        for(int j=1;j<x;j++){
+            v[j][i]+=v[j-1][i];
+        }
+    }
+    return;
+}
+
+// int main(){
+//     int r,c;
+//     cin>>r>>c;
+//     vector <vector<int>> matrix(r, vector<int> (c));
+//     for(int i=0;i<r;i++){
+//         for(int j=0;j<c;j++){
+//             cin>>matrix[i][j];
+//         }
+//     }
+//     int res;
+//     int temp;
+//     vector <vector<int>> resV(r,vector<int> (c));
+//     resV=preSumRow(matrix);
+//     preSumColumn(resV);
+//     do{
+//         cout<<"l1 r1 :";
+//         int l1,r1,l2,r2;
+//         cin>>l1>>r1;
+//         cout<<"l2 r2: ";
+//         cin>>l2>>r2;
+//         // l1--; r1--; l2--; r2--;
+
+//         // for(int i=l1;i<=l2;i++){
+//         //     if(!(r1==0)){
+//         //         res+=resV[i][r2]-resV[i][r1-1];
+//         //     }else{
+//         //         res+=resV[i][r2];
+//         //     }
+//         // }
+
+//         // if(l2>l1 && r2>r1){//logic needs to be updated(this one holds !(l2>l1 && r2>r1) possibilities)
+//         //     res=resV[l2][r2]-resV[l1][r1];
+//         // }else if(l2>l1 && r1>r2){
+//         //     res=resV[l2][r1]-resV[l1][r2];
+//         // }else if(l1>l2 && r2>r1){
+//         //     res=resV[l1][r2]-resV[l2][r1];
+//         // }else{
+//         //     res=resV[l1][r1]-resV[l2][r2];
+//         // }
+//         if(l1>0 && r1>0){
+//             res=resV[l2][r2]-resV[l1-1][r2]-resV[l2][r1-1]+resV[l1-1][r1-1];
+//         }else if(l1==0 && r1!=0){
+//             res=resV[l2][r2]-resV[l2][r1-1];
+//         }else if(l1!=0 && r1==0){
+//             res=resV[l2][r2]-resV[l1-1][r2];
+//         }else{
+//             res=resV[l2][r2];
+//         }
+
+//         cout<<"sum is : "<<res<<endl;
+//         cout<<"wannacontinue? ";
+//         cin>>temp;
+//     }while(temp==1);
+//     // int res=rectangleSum(matrix,l1,r1,l2,r2);
+
+//     return 0;
+// }
+
