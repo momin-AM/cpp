4fd1a2665f87c0a28bf28b99488e144ed109c063
Momin <sk4549151@gmail.com>
2025-10-22 15:45:49 +0530

    Update recursion.cpp

--- exercises/recursion.cpp
+++ exercises/recursion.cpp
@@ -49,3 +49,59 @@ int main(){
     cout<<res<<endl;
     return 0;
 }
+
+//recursion  -DAY 2
+#include <iostream>
+using namespace std;
+
+//recursion on arrays---------
+
+//traversing
+
+void traverse(int arr[],int n){
+    if( n==5){
+        return;
+    }
+    cout<<arr[n]<<endl;
+    traverse(arr,n+1);
+}
+
+int main(){
+    int arr[5]={1,2,3,4,5};
+    traverse(arr,0);
+    return 0;
+}
+
+//print the max val of an array
+#include <limits.h>
+int getMax(int arr[],int ind,int size){
+    if(ind==size-1) return arr[ind];
+    // return getMax(arr,ind+1,size,arr[ind]>max?arr[ind]:max);
+    return max(arr[ind],getMax(arr,ind+1,size));
+}
+
+int main(){
+    int arr[5]={3,10,11,2,5};
+    int max=INT_MIN;
+    int size=sizeof(arr)/sizeof(arr[0]);
+    int res=getMax(arr,0,size);
+    cout<<res<<endl;
+    return 0;
+
+}
+
+//find sum of the elements of array
+
+int getSum(int arr[],int ind,int size){
+    if(ind==size-1) return arr[ind];
+    return arr[ind]+getSum(arr,ind+1,size);
+}
+
+int main(){
+    int arr[5]={1,2,3,4,7};
+    int size=sizeof(arr)/sizeof(arr[0]);
+    int res=getSum(arr,0,size);
+    cout<<res<<endl;
+    return 0;
+}
+
