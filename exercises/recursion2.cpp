4813d9f87f82e1f66541b1cee342d83ce685d868
Momin <sk4549151@gmail.com>
2025-10-28 17:53:41 +0530

    Update recursion2.cpp

--- exercises/recursion2.cpp
+++ exercises/recursion2.cpp
@@ -74,3 +74,74 @@ int main(){
     cout<<res<<endl;
     return 0;
 }
+
+//recursion  -6
+#include <iostream>
+using namespace std;
+
+// //n integers, target int x, array a. return 1 if x in a
+#include <vector>
+
+bool isPresent(vector <int> &arr,int size, int target,int i){
+    if(i==size){
+        return 0;
+    }
+    return (arr[i]==target?1:0) || isPresent(arr,size,target,i+1);
+
+}
+
+int main(){
+    int n=6, x;
+    vector <int> v(n);
+    for(int i=0;i<n;i++){
+        cin>>v[i];
+    }
+    cin>>x;
+    if(isPresent(v,n,x,0)) cout<<"present"<<endl;
+    else cout<<"no"<<endl;
+    return 0;
+}
+
+//return all subsets of a set
+#include <vector>
+#include <cmath>
+void sumSubSets(int *arr, int ind, int sum, vector<int> &subs, int n){
+    if(ind==n){
+        subs.push_back(sum);
+        return;
+    }
+    sumSubSets(arr,ind+1,sum+arr[ind],subs,n);
+    sumSubSets(arr,ind+1,sum,subs,n);
+}
+int main(){
+    int arr[3]={2,4,5};
+    vector<int> subs;
+    int size= sizeof(arr)/sizeof(arr[0]);
+    
+    int sum=0;
+    sumSubSets(arr,0,sum, subs, size);
+    for(int i=0;i<subs.size();i++){
+        cout<<subs[i]<<" ";
+    }
+    return 0;
+}
+
+
+//mXn grid, rules - go right or down . reach (m-1, n-1) from(0,0)... count successfull paths
+
+int pathsCount(int m, int n, int rows, int columns){
+    if (rows >= m || columns >= n)
+        return 0;
+    if(rows==m-1 && columns==n-1){
+        return 1;
+    }
+    return pathsCount(m,n,rows+1, columns)+pathsCount(m,n,rows, columns+1);;
+}
+
+int main(){
+    int m,n;
+    cin>>m>>n;
+    int res=pathsCount(m,n,0,0);
+    cout<<res<<endl;
+    return 0;
+}
